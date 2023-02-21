eb5c1bc686424d73573420eb26307c97403d063b
DarkMiaou <121044105+DarkMiaou@users.noreply.github.com>
2023-02-21 19:44:11 +0100

    push 1 Mathieu

---
+++ Tiles-Of-Hivnir/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D myRB;
+
+    [SerializeField]
+    private float speed;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = new Vector2(Input.GetAxis("Horizontal")* speed * Time.deltaTime, Input.GetAxis("Vertical")* speed * Time.deltaTime);
+     
+    }
+}
