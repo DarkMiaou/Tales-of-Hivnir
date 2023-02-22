20628e6d56f6e79f79b0bf6110375c68fb11a488
DarkMiaou <121044105+DarkMiaou@users.noreply.github.com>
2023-02-22 01:03:20 +0100

    Push Mathieu 2

--- Tiles-Of-Hivnir/Assets/Scripts/PlayerController.cs
+++ Tiles-Of-Hivnir/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D myRB;
+    private Rigidbody2D myRB;
+    private BoxCollider2D BoxCollider;
+    private Animator myAnim;
+    private Vector3 moveDelta;
 
     [SerializeField]
     private float speed;
@@ -13,13 +16,33 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        BoxCollider = GetComponent<BoxCollider2D>();
+        
         myRB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float X = Input.GetAxisRaw("Horizontal");
+        float Y = Input.GetAxisRaw("Vertical");
+        
+        // Mouvement du personnage
         myRB.velocity = new Vector2(Input.GetAxis("Horizontal")* speed * Time.deltaTime, Input.GetAxis("Vertical")* speed * Time.deltaTime);
-     
+
+        // Animation du personnage (le sprite en fonction de la direction)
+        myAnim.SetFloat("moveX", myRB.velocity.x);
+        myAnim.SetFloat("moveY", myRB.velocity.y);
+        
+        // Idle Animation Condition
+        if (X == 1 || X == -1 || Y == 1 || Y == -1)
+        {
+            myAnim.SetFloat("lastMoveX", X);
+            myAnim.SetFloat("lastMoveY", Y);
+        }
+
+
+
     }
 }
