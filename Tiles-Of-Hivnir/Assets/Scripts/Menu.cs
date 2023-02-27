c72de61797396c00efca1bc2d003615ebe50e0cf
Fletcher33 <kyllianhot@gmail.com>
2023-02-27 19:04:37 +0100

    push des menus peut être ça marche pas

---
+++ Tiles-Of-Hivnir/Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string level_to_load;        
+    
+    public void Start_Game()
+    {
+        SceneManager.LoadScene(level_to_load);
+    }
+
+    public void Start_Tuto() 
+    {
+        SceneManager.LoadScene("Test");
+        //il faudra changer "Test" si on change le nom de la scene
+    }
+}
+    
