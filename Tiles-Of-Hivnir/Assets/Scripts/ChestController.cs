e03091fa2b933b71a14b701632130f206a63cd4c
fabio.remnant <fabio.remnant@epita.fr>
2023-02-27 22:40:43 +0100

    interaction system add

---
+++ Tiles-Of-Hivnir/Assets/Scripts/ChestController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChestController : MonoBehaviour
+{
+    public bool isOpen;
+    
+    public void OpenChest()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            Debug.Log("Chest is open");
+        }
+    }
+    public void CloseChest()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            Debug.Log("Chest is closed");
+        }
+    }
+
+}
+
+
---
+++ Tiles-Of-Hivnir/Assets/Scripts/interactable.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class interactable : MonoBehaviour
+{
+
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange) // si inRange
+        {
+            if (Input.GetKeyDown(interactKey))  // si F appuyé
+            {
+                interactAction.Invoke(); // lancer l action
+            }
+            
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player is in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player is not in range");
+        }
+    }
+}
