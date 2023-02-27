a0b876f6a059d2ebb72ef0e57d09dec63e32758a
Aznred <colingomez26@gmail.com>
2023-02-27 16:35:14 +0100

    Changement du mouvement du joueur pour une grid reste à refaire l'animation + certaines collisions de la grid pour qu'il n'y ait plus de bug

--- Tiles-Of-Hivnir/Assets/Scripts/PlayerController.cs
+++ Tiles-Of-Hivnir/Assets/Scripts/PlayerController.cs
@@ -1,51 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D myRB;
-    private BoxCollider2D BoxCollider;
-    private Animator myAnim;
-    private Vector3 moveDelta;
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public LayerMask whatStopMovement;
+    public Animator anim;
 
-    [SerializeField]
-    private float speed;
-    
-    //Item
-    public Item[] Inventory;
-   
-    
     // Start is called before the first frame update
     void Start()
     {
-        BoxCollider = GetComponent<BoxCollider2D>();
-        
-        myRB = GetComponent<Rigidbody2D>();
-        myAnim = GetComponent<Animator>();
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float X = Input.GetAxisRaw("Horizontal");
-        float Y = Input.GetAxisRaw("Vertical");
-        
-        // Mouvement du personnage
-        myRB.velocity = new Vector2(Input.GetAxis("Horizontal")* speed * Time.deltaTime, Input.GetAxis("Vertical")* speed * Time.deltaTime);
-
-        // Animation du personnage (le sprite en fonction de la direction)
-        myAnim.SetFloat("moveX", myRB.velocity.x);
-        myAnim.SetFloat("moveY", myRB.velocity.y);
-        
-        // Idle Animation Condition
-        if (X == 1 || X == -1 || Y == 1 || Y == -1)
+        transform.position = Vector3.MoveTowards(transform.position,movePoint.position,moveSpeed*Time.deltaTime);
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            myAnim.SetFloat("lastMoveX", X);
-            myAnim.SetFloat("lastMoveY", Y);
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f),
+                        .2f, whatStopMovement))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            } 
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+                {
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical") ,0f ),.2f,
+                            whatStopMovement))
+                    {
+                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    }
+                }
+            anim.SetBool("moving",false);
+            
         }
-
-
-
+        else
+        {
+            anim.SetBool("moving", true);
+        }
     }
 }
