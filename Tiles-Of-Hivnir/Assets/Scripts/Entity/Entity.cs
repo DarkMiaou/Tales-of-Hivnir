037313865a42dc897f2ed33b3f8ea89360f904bd
DarkMiaou <121044105+DarkMiaou@users.noreply.github.com>
2023-02-28 18:29:07 +0100

    aaa

---
+++ Tiles-Of-Hivnir/Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TalesofHivnir
+{
+    public class Entity : ScriptableObject
+    {
+        public string Name;
+        public int Age;
+        public string Class;
+        public string Occupation;
+        public int Level;
+        public int Health;
+        public int Strength;
+        public int Magic;
+        public int Defense;
+        public int Speed;
+        public int Damage;
+        public int Armor;
+        public int NoOfAttack;
+        public string Weapon;
+        public Vector2 Postion;
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
+
--- Tiles-Of-Hivnir/Assets/Scripts/GameManager.cs
+++ Tiles-Of-Hivnir/Assets/Scripts/GameManager.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace TalesofHivnir
 {
-    public class GameManager : MonoBehaviour
+    public class GameManager : PlayerController
     {
         public static GameManager instance;
 
