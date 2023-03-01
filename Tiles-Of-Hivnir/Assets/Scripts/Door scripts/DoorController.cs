131768a3fafc8897dcf3fe35d8cca14c5d661abe
fabio.remnant <fabio.remnant@epita.fr>
2023-03-01 22:27:35 +0100

    door add

---
+++ Tiles-Of-Hivnir/Assets/Scripts/Door scripts/DoorController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TalesofHivnir;
+using TalesofHivnir.interactable;
+using TalesofHivnir.Items;
+
+
+namespace TalesofHivnir
+{
+    public class DoorController : MonoBehaviour
+    {
+        public bool isOpen;
+        
+    
+        public void OpenDoor()
+        {
+            if (!isOpen)
+            {
+                isOpen = true;
+                Debug.Log("Door is open");
+            }
+        }
+        
+        public void CloseDoor()
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+                Debug.Log("door closed");
+            }
+        }
+
+    }
+}
+
+
+
+
+
