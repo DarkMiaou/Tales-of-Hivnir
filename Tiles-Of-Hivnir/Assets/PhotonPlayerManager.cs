5782bcf9e288febc4480bff194f92683e1b5c1dd
Aznred <colingomez26@gmail.com>
2023-04-01 20:31:24 +0200

    debut de la map + multi qui est pas encore synchro

---
+++ Tiles-Of-Hivnir/Assets/PhotonPlayerManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using System.Collections.Generic;
+
+public class PhotonPlayerManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject playerPrefab;
+    private List<GameObject> players = new List<GameObject>();
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0.5f,0.5f,0), Quaternion.identity);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            GameObject newPlayerObject = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+            players.Add(newPlayerObject);
+            newPlayerObject.GetComponent<PhotonView>().TransferOwnership(newPlayer);
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            foreach (GameObject playerObject in players)
+            {
+                if (playerObject.GetComponent<PhotonView>().Owner == otherPlayer)
+                {
+                    players.Remove(playerObject);
+                    PhotonNetwork.Destroy(playerObject);
+                    break;
+                }
+            }
+        }
+    }
+}
