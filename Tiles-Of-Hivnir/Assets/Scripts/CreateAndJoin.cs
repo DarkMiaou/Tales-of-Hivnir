00489fd8d72167b33d41c80b1c525bc2b1277209
Aznred <colingomez26@gmail.com>
2023-04-01 13:58:56 +0200

    Debut du multi marche mais pas encore la synchro pour voir les autres joueurs

---
+++ Tiles-Of-Hivnir/Assets/Scripts/CreateAndJoin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CreateAndJoin : MonoBehaviourPunCallbacks
+{
+   public InputField input_create;
+   public InputField input_join;
+
+   public void CreateRoom()
+   {
+      PhotonNetwork.CreateRoom(input_create.text, new RoomOptions(){MaxPlayers = 4, IsVisible = true, IsOpen = true});
+   }
+
+   public void JoinRoom()
+   {
+      PhotonNetwork.JoinRoom(input_join.text);
+   }
+
+   public override void OnJoinedRoom()
+   {
+      PhotonNetwork.LoadLevel("Test");
+   }
+}
---
+++ Tiles-Of-Hivnir/Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate("Player",new Vector3(0.5f,0.5f,0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
