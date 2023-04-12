51de0e7b73fb4c418e619cd915c0f62fa27ce13f
Aznred <colingomez26@gmail.com>
2023-04-12 10:28:26 +0200

    fix des portes elles marchent mais le joueur est buggé
    sf

--- Tiles-Of-Hivnir/Assets/DoorController.cs
+++ Tiles-Of-Hivnir/Assets/DoorController.cs
@@ -10,28 +10,5 @@ public class DoorController : MonoBehaviour
     public float distance = 0.2f;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        if (isOut == false)
-        {
-            destination = GameObject.FindGameObjectWithTag("doordunout").GetComponent<Transform>();
-        }
-        else 
-        {
-            destination = GameObject.FindGameObjectWithTag("doordun").GetComponent<Transform>();
-        }
-    }
-
-    public void usedoor(Collider2D other)
-    {
-        Debug.Log("space pressed");
-        other.transform.position = new Vector2 (destination.position.x, destination.position.y);
-        isOut = !isOut;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
---
+++ Tiles-Of-Hivnir/Assets/door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TalesofHivnir;
+using TalesofHivnir.Entities;
+using UnityEngine;
+
+public class door : MonoBehaviour
+
+{
+  
+    
+    public GameObject player;
+
+    // Définir une position de destination pour le joueur
+    public Vector2 destination;
+
+    void Start()
+    {
+        // Chercher le joueur et stocker sa référence dans la variable "player"
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            StartCoroutine(teleport());
+        }
+    }
+
+    IEnumerator  teleport()
+    {
+        yield return new WaitForSeconds(1);
+        player.transform.position = new Vector3(destination.x, destination.y ,0);
+        player.GetComponent<PlayerController>().transform.position = player.transform.position;
+        
+    } 
+}
+
