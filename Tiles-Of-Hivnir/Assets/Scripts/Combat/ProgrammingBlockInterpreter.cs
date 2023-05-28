3892ec770ad78daabbd2c56c5ef17dcaeb5e0b40
Aznred <colingomez26@gmail.com>
2023-05-28 21:25:15 +0200

    Joueur à des PA

--- Tiles-Of-Hivnir/Assets/Scripts/Combat/ProgrammingBlockInterpreter.cs
+++ Tiles-Of-Hivnir/Assets/Scripts/Combat/ProgrammingBlockInterpreter.cs
@@ -36,8 +36,8 @@ namespace TalesofHivnir
         public RectTransform startBlock;
 
         public GameObject gameoverUI;
-        public int PA;
-        public int PA_;
+        public int PA= 0;
+        public int Max_PA;
         
 
         private Button ClearButton;
@@ -51,7 +51,7 @@ namespace TalesofHivnir
 
         private void Start()
         {
-            PA_ = PA;
+            Max_PA = 3;
             //playercurrenthealth = Player.Health;
             Debug.Log(Player.instance.GetCurrentHealth());
             SetStatsEnemy();
@@ -83,9 +83,9 @@ namespace TalesofHivnir
                 block.Execute();
                 yield return new WaitForSeconds(blockExecutionDelay);
             }
-
+            PA = 0;
             ClearBlocks();
-            PA = PA_;
+          
         }
 
         public void AddBlock(ProgrammingBlock block)
@@ -95,27 +95,30 @@ namespace TalesofHivnir
             if (!blockList.Contains(block))
             {
                 blockList.Add(block);
-                PA--;   
+                PA++;   
             }
         }
 
-     
+        public void Remove(ProgrammingBlock block)
+        {
+            Destroy(Blocks[Blocks.Count-1].gameObject);
+            Blocks.Remove(Blocks[Blocks.Count-1]);
+            blockList.Remove(block);
+            PA--;
+        }
 
         private void Update()
         {
+            Debug.Log(Blocks.Count);
             coordx = playcoord.position.x - mobcoord.position.x;
             coordy = playcoord.position.y - mobcoord.position.y;
             timer -= Time.deltaTime;
-            //int index = blockList.Count;
+            int index = blockList.Count -1;
            
-            /*if (PA <0)
-            //{
-                Blocks.Add(startBlock);
-                Destroy(Blocks[index].gameObject);
-                blockList.RemoveAt(index);
-
-                PA++;
-            } Marche à moitié*/
+            if (PA > Max_PA)
+            {
+                Remove(blockList[index ]);
+            }
                
           
             if (timer <= 0f || isInterpreting)
@@ -242,10 +245,13 @@ namespace TalesofHivnir
             for (int i = 1; i < Blocks.Count; i++)
             {
                 Destroy(Blocks[i].gameObject);
+                PA = 0;
             }
             blockList = new List<ProgrammingBlock>();
             Blocks = new List<RectTransform>();
             Blocks.Add(startBlock);
+           
+
         }
 
         public void gameOver()
